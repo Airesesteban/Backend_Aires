7eb92e5e39636c5087cbbcc53cc5a1ced3860b26
Airesesteban <airesesteban@gmail.com>
2024-01-01 08:12:18 -0300

    fix import message model

--- src/routes/dbMessages.routes.js
+++ src/routes/dbMessages.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import messageModel from "../dao/models/message.models.js";
+import messageModel from "../dao/models/message.model.js";
 
 const router = Router();
 
